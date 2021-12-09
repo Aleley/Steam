0bc76a327d1331a21c668045bed3d22e61d327ee
Ale <aleley@live.it>
2021-12-09 11:51:48 +0100

    Initial Commit

---
+++ Steam App/Steam_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Steam_AppApp.swift
+//  Steam App
+//
+//  Created by Alessandra Coppola on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Steam_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
